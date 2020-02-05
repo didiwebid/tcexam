2fbe7ca2a6d5a7bf02db86bb3c92208a59169611
Didi Wahyudi <60571533+didiwebid@users.noreply.github.com>
2020-02-05 09:22:44 +0700

    HTML tag

--- public/code/tce_test_execute.php
+++ public/code/tce_test_execute.php
@@ -91,9 +91,9 @@ if (isset($_REQUEST['testid']) and ($_REQUEST['testid'] > 0)) {
             F_terminateUserTest($test_id);
             // redirect the user to the index page
             header('Location: index.php');
-            echo '<'.'?xml version="1.0" encoding="'.$l['a_meta_charset'].'"?'.'>'.K_NEWLINE;
-            echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'.K_NEWLINE;
-            echo '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="'.$l['a_meta_language'].'" lang="'.$l['a_meta_language'].'" dir="'.$l['a_meta_dir'].'">'.K_NEWLINE;
+            //echo '<'.'?xml version="1.0" encoding="'.$l['a_meta_charset'].'"?'.'>'.K_NEWLINE;
+            echo '<!DOCTYPE html>'.K_NEWLINE;
+            echo '<html lang="'.$l['a_meta_language'].'" dir="'.$l['a_meta_dir'].'">'.K_NEWLINE;
             echo '<head>'.K_NEWLINE;
             echo '<title>REDIRECT</title>'.K_NEWLINE;
             echo '<meta http-equiv="refresh" content="0;url=index.php" />'.K_NEWLINE; //reload page
@@ -218,9 +218,9 @@ if (isset($_REQUEST['testid']) and ($_REQUEST['testid'] > 0)) {
     } else {
         // redirect the user to the index page
         header('Location: index.php');
-        echo '<'.'?xml version="1.0" encoding="'.$l['a_meta_charset'].'"?'.'>'.K_NEWLINE;
-        echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'.K_NEWLINE;
-        echo '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="'.$l['a_meta_language'].'" lang="'.$l['a_meta_language'].'" dir="'.$l['a_meta_dir'].'">'.K_NEWLINE;
+        //echo '<'.'?xml version="1.0" encoding="'.$l['a_meta_charset'].'"?'.'>'.K_NEWLINE;
+        echo '<!DOCTYPE html>'.K_NEWLINE;
+        echo '<html lang="'.$l['a_meta_language'].'" dir="'.$l['a_meta_dir'].'">'.K_NEWLINE;
         echo '<head>'.K_NEWLINE;
         echo '<title>REDIRECT</title>'.K_NEWLINE;
         echo '<meta http-equiv="refresh" content="0;url=index.php" />'.K_NEWLINE; //reload page
